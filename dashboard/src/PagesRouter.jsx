056e1d0d299f629fb6ceddd1cd541b3268711391
Andres Restrepo <andres613@misena.edu.co>
2023-05-26 00:02:43 -0500

    dashboard base files added

---
+++ dashboard/src/PagesRouter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import styles from './PagesRouter.module.css';
+
+
+export const PagesRouter = () => {
+
+    return (
+        <>
+        <Router>
+            <Routes>
+            </Routes>
+        </Router>
+        </>
+    )
+}
+
+
+
---
+++ dashboard/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { PagesRouter } from './PagesRouter.jsx';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <PagesRouter />
+    </React.StrictMode>
+);
