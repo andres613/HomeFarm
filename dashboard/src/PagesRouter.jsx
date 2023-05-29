1a793ffe522faa8c227afd6e9e0534fc371efacf
Andres Restrepo <andres613@misena.edu.co>
2023-05-28 22:16:41 -0500

    The style of the PagesRouter component is fixed

--- dashboard/src/PagesRouter.jsx
+++ dashboard/src/PagesRouter.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
-import styles from './PagesRouter.module.css';
+import "./PagesRouter.module.css";
 
 import { Login } from './components/Login'
 
