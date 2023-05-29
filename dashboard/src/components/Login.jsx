7871b86b222683776775f6e0150215cbb6b68b01
Andres Restrepo <andres613@misena.edu.co>
2023-05-29 00:20:38 -0500

    Cookies manager added

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { get } from "../utils/httpClient.js"
+import Cookies from 'universal-cookie';
 import styles from './Login.module.css';
+
+const cookies = new Cookies();
 
 export const Login = () => {
 
@@ -9,7 +13,25 @@ export const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const startSession = await get(email, password);
+
+        if(startSession) {
+            cookies.set('id', startSession.id, { path: "/", sameSite: "lax" }); //accesible desde todos lados
+            cookies.set('name', startSession.name, { path: "/", sameSite: "lax" });
+            cookies.set('username', startSession.username, { path: "/", sameSite: "lax" });
+            cookies.set('email', startSession.email, { path: "/", sameSite: "lax" });
+            window.location.href="./dashboard";
+        } else {
+            alert("Email o password incorrecto!!")
+        }
     }
+
+    useEffect(() => {
+        if(cookies.get('username')) {
+            window.location.href='./dashboard';
+        }
+    })
 
     return (
         <div className={styles.login} >
