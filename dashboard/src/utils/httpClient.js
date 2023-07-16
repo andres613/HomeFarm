73861c03d7bd71c5c3fdfb6a9963e2f046797cf7
Andres Restrepo <andres613@misena.edu.co>
2023-07-15 22:01:57 -0500

    API mock login

--- dashboard/src/utils/httpClient.js
+++ dashboard/src/utils/httpClient.js
@@ -2,17 +2,28 @@ const baseUrl = 'http://localhost:8080';
 let data = "";
 
 export async function login(email, pass) {
-    data = `{"id":"id", "name":"name", "phone": "phone", "email":"${email}", "pass":"${pass}"}`; 
-
-    return await fetch(baseUrl + "/login", {
-        method: 'POST',
-        body: data,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+    return await fetch("./FakeApiLogin.json")
         .then(apiResponse => apiResponse.json())
-        .catch(error => false)
+        .then(response => {
+            for (let i = 0; i < response.length; i++) {
+                if (email === response[i].email && pass === response[i].password) {
+                    return response[i];
+                }
+            }
+            return false;
+        });
+    
+    // data = `{"id":"id", "name":"name", "phone": "phone", "email":"${email}", "pass":"${pass}"}`; 
+
+    // return await fetch(baseUrl + "/login", {
+    //     method: 'POST',
+    //     body: data,
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+    //     .then(apiResponse => apiResponse.json())
+    //     .catch(error => false)
 }
 
 export function apiResponse(arg) {
